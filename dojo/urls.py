05d94a9aa51b4edada8c0fc11bf8b43954f128ca
Lee hyunjoo <siwabada@gmail.com>
2017-03-18 18:46:49 +0900

    dojo.views.mysum 에서 다수숫자를 받도록 개선,  dojo.views.myname에서 이름과, 나이 인자를 받도록 개선

--- dojo/urls.py
+++ dojo/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum),
-    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', views.mysum),
-    url(r'^sum/(?P<x>\d+)/$', views.mysum),
+    # url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum),
+    # url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', views.mysum),
+    # url(r'^sum/(?P<x>\d+)/$', views.mysum),
+    url(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
+    url(r'^hello/(?P<name>[ㄱ-힣]{2,3})/(?P<age>\d{1,3})/$', views.myname),
 ]
--- dojo/views.py
+++ dojo/views.py
@@ -2,5 +2,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def mysum(request, x, y=0, z=0):
-    return HttpResponse(int(x) + int(y) + int(z))
+# def mysum(request, x, y=0, z=0):
+#     return HttpResponse(int(x) + int(y) + int(z))
+
+def mysum(request, numbers):
+    return HttpResponse(sum(map(lambda s: int(s or 0),numbers.split('/'))))
+
+def myname(request, name, age):
+    return HttpResponse('안녕하세요. {}. {}살이시네요'.format(name,age))
