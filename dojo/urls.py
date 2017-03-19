45e0b81ef37d902113e07168827019b544e605b3
Lee hyunjoo <siwabada@gmail.com>
2017-03-19 17:36:17 +0900

    함수 기반 뷰(FBV) 의 4가지 패턴

--- dojo/urls.py
+++ dojo/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     # url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', views.mysum),
     # url(r'^sum/(?P<x>\d+)/$', views.mysum),
     url(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
-    url(r'^hello/(?P<name>[ㄱ-힣]{2,3})/(?P<age>\d{1,3})/$', views.myname),
+    url(r'^hello/(?P<name>[ㄱ-힣]{2,3})/(?P<age>\d{1,3})/$', views.hello),
+    url(r'^list1/$', views.post_list1),
+    url(r'^list2/$', views.post_list2),
+    url(r'^list3/$', views.post_list3),
+    url(r'^excel/$', views.excel_download),
+
 ]
--- dojo/views.py
+++ dojo/views.py
@@ -1,6 +1,8 @@
 # dojo/views.py
-from django.http import HttpResponse
+import os
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from mysite import settings
 
 # def mysum(request, x, y=0, z=0):
 #     return HttpResponse(int(x) + int(y) + int(z))
@@ -8,5 +10,38 @@ from django.shortcuts import render
 def mysum(request, numbers):
     return HttpResponse(sum(map(lambda s: int(s or 0),numbers.split('/'))))
 
-def myname(request, name, age):
+
+def hello(request, name, age):
     return HttpResponse('안녕하세요. {}. {}살이시네요'.format(name,age))
+
+
+def post_list1(request):
+    name = '공유'
+    return HttpResponse('''
+    <h1> hello, </h1>
+    <p>{name}</p>
+    <p>반가워요</p>
+    '''.format(name=name))
+
+
+def post_list2(request):
+    name = '홍철'
+    return render(request, 'dojo/post_list.html', {'name': name})
+
+
+def post_list3(request):
+    return JsonResponse({
+        'message' : '안녕 파이썬 장고',
+        'items' : ['파이썬', '장고', 'AWS', 'Azure'],
+    }, json_dumps_params = {'ensure_ascii': True})
+
+def excel_download(request):
+    # filepath = '/Users/Leehyunjoo/GitHub/ask_django/scimagojr-3.xlsx'
+    filepath = os.path.join(settings.BASE_DIR, 'scimagojr-3.xlsx')
+    # 현재 프로젝트 최상위 (부모폴더) 밑에 있는 'scimagojr-3.xlsx' 파일
+    filename = os.path.basename(filepath)
+
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+        return response
