020ed132149f70b3bbd315421da590ecdde7e427
Lee hyunjoo <siwabada@gmail.com>
2017-03-19 20:22:25 +0900

    클래스 기반 뷰(CBV) 의 4가지 패턴

--- dojo/urls.py
+++ dojo/urls.py
@@ -1,6 +1,7 @@
 # dojo/urls.py
 from django.conf.urls import url
 from . import views
+from . import views_cbv
 
 urlpatterns = [
     # url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum),
@@ -8,9 +9,14 @@ urlpatterns = [
     # url(r'^sum/(?P<x>\d+)/$', views.mysum),
     url(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
     url(r'^hello/(?P<name>[ㄱ-힣]{2,3})/(?P<age>\d{1,3})/$', views.hello),
+
     url(r'^list1/$', views.post_list1),
     url(r'^list2/$', views.post_list2),
     url(r'^list3/$', views.post_list3),
     url(r'^excel/$', views.excel_download),
 
+    url(r'^cbv/list1/$', views_cbv.post_list1),
+    url(r'^cbv/list2/$', views_cbv.post_list2),
+    url(r'^cbv/list3/$', views_cbv.post_list3),
+    # url(r'^cbv/excel/$', views_cbv.excel_download),
 ]
---
+++ dojo/views_cbv.py
@@ -0,0 +1,45 @@
+from django.http import HttpResponse, JsonResponse
+from django.views.generic import View, TemplateView
+
+# CBV 사용시에 View를 상속받아서 사용하는 일을 거의 없다
+class PostListView1(View):
+    def get(self, request):
+        name = '공유'
+        html = self.get_template_string().format(name=name)
+        return HttpResponse(html)
+
+    def get_template_string(self):
+        return '''
+                <h1> hello, </h1>
+                <p>{name}</p>
+                <p>반가워요</p>
+                '''
+
+post_list1 = PostListView1.as_view()
+
+
+class PostListView2(TemplateView):
+    template_name = 'dojo/post_list.html'
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['name'] = '공유'
+        return context
+
+post_list2 = PostListView2.as_view()
+
+
+class PostListView3(View):
+    def get(self,request):
+        return JsonResponse(self.get_data(), json_dumps_params={'ensure_ascii': True})
+
+    def get_data(self):
+        return{
+            'message' : '안녕 파이썬 장고',
+            'items' : ['파이썬', '장고', 'AWS', 'Azure'],
+        }
+
+post_list3 = PostListView3.as_view()
+
+class ExcelDownloadView(View):
+    pass
